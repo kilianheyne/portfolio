7bf9940e631e56a93555e75881049c1243a9a48c
Kilian Heyne <kilianheyne@icloud.com>
2025-06-17 17:05:02 +0200

    first iteration of a header.component with HTML and SCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { NavComponent } from "./shared/nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboveTheFoldComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
