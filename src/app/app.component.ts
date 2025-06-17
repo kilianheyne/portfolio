0c92e4e01a079de60dfd943ca1a67e9f671c6b02
Kilian Heyne <kilianheyne@icloud.com>
2025-06-17 17:39:18 +0200

    first iteration of HTML for why-me.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { NavComponent } from "./shared/nav/nav.component";
+import { WhyMeComponent } from "./why-me/why-me.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboveTheFoldComponent, NavComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, NavComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
