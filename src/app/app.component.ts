0152e6b5702da7e4c43621c84974a88ecfea22a3
Kilian Heyne <kilianheyne@icloud.com>
2025-06-17 13:10:33 +0200

    first iteration of HTML for above-the-fold-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboveTheFoldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
