02a603397d8d8e4218279f22cc24d34167e680fa
Kilian Heyne <kilianheyne@icloud.com>
2025-06-18 10:50:01 +0200

    first iteration of HTML for my-projects.component. Will circle back later as I am currently lacking ideas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component
 import { NavComponent } from "./shared/nav/nav.component";
 import { WhyMeComponent } from "./why-me/why-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { MyProjectsComponent } from "./my-projects/my-projects.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboveTheFoldComponent, NavComponent, WhyMeComponent, MySkillsComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, NavComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
