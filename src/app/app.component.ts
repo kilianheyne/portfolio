8039f0072e75218c8f9ef1e5602d3770eb648654
Kilian Heyne <kilianheyne@icloud.com>
2025-06-18 11:28:14 +0200

    first iteration of HTML for colleage references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { NavComponent } from "./shared/nav/nav.component";
 import { WhyMeComponent } from "./why-me/why-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { MyProjectsComponent } from "./my-projects/my-projects.component";
+import { MateRefsComponent } from "./mate-refs/mate-refs.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboveTheFoldComponent, NavComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, NavComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, MateRefsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
