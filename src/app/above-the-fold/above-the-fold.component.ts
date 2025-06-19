774f030d082949c44d70ee5d8c124f6274b5ba85
Kilian Heyne <kilianheyne@icloud.com>
2025-06-19 15:11:15 +0200

    above-the-fold.component now has hover-effects

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AboveTheFoldComponent {
 
+    // #region attributes
+
+    githubIcon: string = 'assets/images/hero-section/contact-icons/github.png';
+    eMailIcon: string = 'assets/images/hero-section/contact-icons/e-mail.png';
+    linkedinIcon: string = 'assets/images/hero-section/contact-icons/linkedin.png';
+  
+    // #endregion
+  
+    // #region methods
+  
+    changeIcon(platform: string, hover: boolean):void {
+      switch (platform) {
+        case ('github'):
+          this.githubIcon = hover ? 'assets/images/hero-section/contact-icons/github-hover.png' : 'assets/images/hero-section/contact-icons/github.png';
+          break;
+        case ('e-mail'):
+          this.eMailIcon = hover ? 'assets/images/hero-section/contact-icons/e-mail-hover.png' : 'assets/images/hero-section/contact-icons/e-mail.png';
+          break;
+        case ('linkedin'):
+          this.linkedinIcon = hover ? 'assets/images/hero-section/contact-icons/linkedin-hover.png' : 'assets/images/hero-section/contact-icons/linkedin.png';  
+          break;
+      }
+    }
+  
+    //#endregion
+
 }
