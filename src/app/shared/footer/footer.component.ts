2c3d01a669b91af366220b1d17c07b19a630947f
Kilian Heyne <kilianheyne@icloud.com>
2025-06-19 14:29:21 +0200

    added hover effects to footer.component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  // #region attributes
+
+  githubIcon: string = 'assets/images/footer/github-icon.png';
+  eMailIcon: string = 'assets/images/footer/e-mail-icon.png';
+  linkedinIcon: string = 'assets/images/footer/linkedin-icon.png';
+
+  // #endregion
+
+  // #region methods
+
+  changeIcon(platform: string, hover: boolean):void {
+    switch (platform) {
+      case ('github'):
+        this.githubIcon = hover ? 'assets/images/footer/github-icon-hover.png' : 'assets/images/footer/github-icon.png';
+        break;
+      case ('e-mail'):
+        this.eMailIcon = hover ? 'assets/images/footer/e-mail-icon-hover.png' : 'assets/images/footer/e-mail-icon.png';
+        break;
+      case ('linkedin'):
+        this.linkedinIcon = hover ? 'assets/images/footer/linkedin-icon-hover.png' : 'assets/images/footer/linkedin-icon.png';  
+        break;
+    }
+  }
+
+  //#endregion
+
 }
