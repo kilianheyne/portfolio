ba36c554b9fce3fb2c36d74ad145f46086a1541b
Kilian Heyne <kilianheyne@icloud.com>
2025-06-24 12:26:42 +0200

    reworking contact form to actually working

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(){
+    console.log(this.contactData);
+  }
 
 }
