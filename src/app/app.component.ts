cb6ca92096b4e3a8da2a1337b02112ba3edef65c
Kilian Heyne <kilianheyne@icloud.com>
2025-06-24 17:36:59 +0200

    added the angular-language-library and started a translation for header, added a changeLanguage-function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,24 @@ import { MyProjectsComponent } from "./my-projects/my-projects.component";
 import { MateRefsComponent } from "./mate-refs/mate-refs.component";
 import { ContactMeComponent } from "./contact-me/contact-me.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboveTheFoldComponent, HeaderComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, MateRefsComponent, ContactMeComponent, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, HeaderComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, MateRefsComponent, ContactMeComponent, FooterComponent, HeaderComponent, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(private translate: TranslateService){}
+
+  changeLanguage(lingo: string){
+    this.translate.use(lingo);
+  }
 
 }
