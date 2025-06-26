6afdc47b235daafc2bacb10049d7f4b452a095ff
Kilian Heyne <kilianheyne@icloud.com>
2025-06-26 15:49:08 +0200

    added an additional hover-effect to contact form

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ProjectComponent } from "./project/project.component";
 import { ProjectsService } from '../shared/services/projects.service';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-projects',
-  imports: [ProjectComponent],
+  imports: [ProjectComponent, TranslatePipe, TranslateDirective],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
