4867a5d7f51856639112214aa89ed8bee1737bbf
Kilian Heyne <kilianheyne@icloud.com>
2025-06-30 14:14:23 +0200

    added local storage for choosen language

--- src/app/shared/mobile-header/mobile-header.component.ts
+++ src/app/shared/mobile-header/mobile-header.component.ts
@@ -24,7 +24,16 @@ export class MobileHeaderComponent {
   // #endregion
   // #region constructor
 
-  constructor(private translate: TranslateService){} //ngx-library for translation
+  constructor(private translate: TranslateService){
+
+    const savedLingo = localStorage.getItem('prefLingo');
+    if (savedLingo) {
+      this.translate.use(savedLingo);
+      this.activeLanguage = savedLingo;
+    } else {
+      this.translate.use(this.activeLanguage);
+    }
+  } //ngx-library for translation
 
   // #endregion
   // #region methods
@@ -32,6 +41,7 @@ export class MobileHeaderComponent {
   changeLanguage(lingo: string){ //button in header have "lingo" 'de' or 'en'
     this.translate.use(lingo); // chosing the needed json for translation in ngx-translate
     this.activeLanguage = lingo; // caching language
+    localStorage.setItem('prefLingo', lingo);
   }
 
   setActiveNav (navTag: string){
