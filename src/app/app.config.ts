1caca8061c68e50f0f221f2400d7cd53211336cf
Kilian Heyne <kilianheyne@icloud.com>
2025-07-01 13:33:51 +0200

    fixed an issue with reloading policy-page and scrolling-behaviour

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import {provideHttpClient} from "@angular/common/http";
 import {provideTranslateService, TranslateLoader} from "@ngx-translate/core";
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
@@ -12,7 +12,12 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: "top",
+      })
+    ),
     provideHttpClient(),
     provideTranslateService({
       loader: {
